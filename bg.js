35458b31fd07beea0e7e6cf10da8f3bb3d4d0179
arrC <naveen@arrc.in>
2016-08-15 12:03:37 +0530

    fixed missing favicon bug

--- bg.js
+++ bg.js
@@ -1,30 +1,29 @@
 // arrC . naveen@arrc.in . arrc.in
+// lot of room refactoring here
+// data extraction is very fragile eg., faviconurl, title which may no exist
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.name === 'bloiPageActionClicked') {
       // console.log("Message", message);
 
       // Extract data from message object
       var data = message.data[0];
-      var _favIcon = data.favIconUrl.split('?')[0];
       var _title = data.title;
       var _url = data.url;
+      // https://toddmotto.com/methods-to-determine-if-an-object-has-a-given-property/#prop-in-myobject
+      var _favIcon = 'favIconUrl' in data ? data.favIconUrl.split('?')[0] : "noicon";
 
-      // Prepare querified url
-      var _query = "url=" + _url + "&"
-      _query += "title=" + _title + "&"
-      _query += "favIcon=" + _favIcon
-
-      // Encode the url
-      var encodedUrl = encodeURIComponent(_query); // rails doesn't like this.
+      var _query = "url=" + encodeURIComponent(_url) + "&"
+      _query += "title=" + encodeURIComponent(_title) + "&"
+      _query += "favIcon=" + encodeURIComponent(_favIcon)
 
       // These are left here for debugging purposes ofcourse. Don't judge me.
       // console.log("Full url \n", _query);
-      // console.log("Encoded url \n", encodedUrl);
-      // console.log("Decoded url \n", decodeURIComponent(encodedUrl));
 
       // Open new bookmarks page with query string
       chrome.tabs.create({
-          'url': "http://localhost:3000/bookmarks/new?" + _query
+          // 'url': "http://localhost:3000/bookmarks/new?" + _query
+          'url': "http://bloi.co/bookmarks/new?" + _query
       }, function(tab) {
         // You can sleep here or booze or whatever.
       });
